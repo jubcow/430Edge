88536df3288aa19c95e62436fb3a20e2116009d0
Justin <jadams5@mail.umw.edu>
2020-03-28 17:04:46 -0400

    added redirection for homepage and student

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Link, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import HomePage from './containers/Homepage/HomePage.js';
 import StudentPage from './containers/Student Page/StudentPage';
 
 // redirects to different pages
-function App() {
-  return (
-    <div>
-      <StudentPage />
-    </div>
-  );
+class App extends Component {
+
+  render () {
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/student" component={StudentPage} />
+      </Switch>
+    );
+
+    return (
+      <div>
+          {routes}
+      </div>
+    );
+    
+  }
 }
 
 export default App;
--- src/components/Progress Tree/ProgressTree.js
+++ src/components/Progress Tree/ProgressTree.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const ProgressTree = (props) => {
+    
+
+    const treeProgress = (props) => (
+        // tree image determined here
+        <h1>Tree Image</h1>
+    );
 
     return (
         <div>
             <h1>Progress Tree</h1>
+            {treeProgress()}
         </div>
     )
 }
