445589ca30cab4758819147c40b47daf16eec2a4
root <root@instance-1.us-central1-a.c.cpsc430-272902.internal>
2020-04-06 20:47:00 +0000

    Testing backend connection, able to print mysql data to console but not in JSX

--- src/containers/Homepage/HomePage.js
+++ src/containers/Homepage/HomePage.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import StudentLayout from '../../hoc/Student Layout/StudentLayout';
+//import StudentLayout from '../../hoc/Student Layout/StudentLayout';
 
 import "./HomePage.css";
 //import "./woodard.jpg";
+var d;
+var d2;
+var testPrint = "testing123";
+
+var p = fetch('http://35.192.57.209:8000/test').then((response) => {return response.json();}).then((data) => {
+    	console.log(data);
+	d = data;
+  });
+
+var p2 = fetch('http://35.192.57.209:8000/test').then((response) => {return response.text();}).then((data) => {
+	console.log(data);
+	d2 = data;
+});
+console.log("d data: "+d);
+console.log("d2 data: "+d2);
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
+	state = {
+		info: []
+	}
+
+	getInfo = _ => {
+		fetch('http://35.192.57.209:8000/test')
+			.then((response) => {return response.json();})
+			.then((response) => this.setState({info: response.data}));
+	}
+/*
+    constructor(props) {
+    	super(props);
+	this.state = {
+            info: ''
+        }
+    }
+*/
     render() {
-        return (
-		<body>
-            <div id="parent">
+	const{ info } = this.state;
+        return (		
+            <div className="HomePage">
         <ul>
-                <center><div class="white"><h1>The EDGE Program</h1></div></center>
+                <center><div className="white"><h1>The EDGE Program</h1></div></center>
                 <li><a href="/Login">Login</a></li>
                 <li><a href="/About">About</a></li>
         </ul>
@@ -24,11 +53,11 @@ class HomePage extends React.Component {
 		<br></br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 		<br></br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 		<br></br>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</center>
-        
-	
+		<br></br>	
+		{ JSON.stringify(this.info) }	{info} {this.info}	{testPrint}
+		{d} {d2} {p[1]} {JSON.stringify(p)} {JSON.stringify(p[0])} {p2[0]} 
+	</center>	
         </div>
-	</body>
         )
     }
 }
