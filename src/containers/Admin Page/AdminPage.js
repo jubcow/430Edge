60b1d44b2bed51a3b2f4f9676dcd13ef6b3c6498
root <root@instance-1.us-central1-a.c.cpsc430-272902.internal>
2020-04-17 23:41:08 +0000

    Frontend functionality of the create account should be done, just need to create a backend function to handle it

--- src/containers/Admin Page/AdminPage.js
+++ src/containers/Admin Page/AdminPage.js
@@ -6,7 +6,11 @@ class AdminPage extends React.Component {
         constructor(props) {
                 super(props);
                 this.state = {
-                        info: []
+                        info: [],
+			fullname : "",
+			umwid : "",
+			email : "",
+			accType : ""
                 }
         }
 
@@ -29,15 +33,15 @@ class AdminPage extends React.Component {
                 return (
                 <div className="AdminPage">
 			<div className="row">
-                                <div class ="column" style={{backgroundColor : '#aaa'}}>
+                                <div className ="column" style={{backgroundColor : '#aaa'}}>
 					{this.state.info.map(info => <div>{info.UMWID}</div>)}
 					
 				</div>
-				<div class ="column" style={{backgroundColor : '#bbb'}}>
+				<div className ="column" style={{backgroundColor : '#bbb'}}>
 					{this.state.info.map(info => <div>{info.Fullname}</div>)}
 					
 				</div>
-				<div class ="column" style={{backgroundColor : '#ccc'}}>
+				<div className ="column" style={{backgroundColor : '#ccc'}}>
 					{this.state.info.map(info => <div>
 		
 						<form onSubmit={this.onFormSubmit}>
@@ -60,24 +64,25 @@ class AdminPage extends React.Component {
                         <form>
                         	<label>
                                 	Full Name :
-                                       	<input type="text" name="fullname" />
+                                       	<input type="text" name="fullname" onChange={this.handleInputChange.bind(this)}/>
                                 </label>
 				<br></br>
 				<label>
 					UMW ID :
-					<input type="text" name="umwid" />
+					<input type="text" name="umwid" onChange={this.handleInputChange.bind(this)}/>
 				</label>
 				<br></br>
 				<label>
                                         Acc. Type :
-                                        <input type="text" name="accountType" />
+                                        <input type="text" name="accType" onChange={this.handleInputChange.bind(this)}/>
                                 </label>
 				<br></br>
 				<label>
                                         Email :
-                                        <input type="text" name="email" />
+                                        <input type="text" name="email" onChange={this.handleInputChange.bind(this)}/>
                                 </label>
-                               	<input type="submit" value="Submit" />
+				<br></br>
+                               	<input type="submit" value="Submit" onClick={this.handleSubmit.bind(this)}/>
                         </form>
 
              	</div>
@@ -89,17 +94,44 @@ class AdminPage extends React.Component {
 		var str = "http://35.192.57.209:8000/approve";
 
 		console.log(id)
-	const header = {'Accept' : "application/json", "Content-Type": "application/x-www-form-urlencoded"};
-        const searchParams = new URLSearchParams({var1: id});
+		const header = {'Accept' : "application/json", "Content-Type": "application/x-www-form-urlencoded"};
+        	const searchParams = new URLSearchParams({var1: id});
 
-        return fetch(`http://35.192.57.209:8000/approve`,
-        {method: "POST",
-         headers: header,
-         body: searchParams }).then(function(resp){
-             return resp.json();
-         });
+        	return fetch(`http://35.192.57.209:8000/approve`,
+        	{method: "POST",
+         	headers: header,
+         	body: searchParams }).then(function(resp){
+             		return resp.json();
+         	});
+	}
 
-	}	
+	handleInputChange(event) { //this will be called onChange for the text fields on account creation
+		let self = this;
+    		const target = event.target;
+    		const name = target.name;
+		const value = target.value;
+
+		console.log("Name: "+ name + " --- Value: " + value); 
+		//commenting out the below setstate and removing the () => from the onChanges allowed these to print normally.
+    		self.setState({
+      			[name] : value
+    		});
+		
+		console.log("Fullname: " + this.state.fullname + " UMW ID: " + this.state.umwid);
+		
+  	}
+
+	handleSubmit(event) {
+                const header = {'Accept' : "application/json", "Content-Type": "application/x-www-form-urlencoded"};
+                const searchParams = new URLSearchParams({fullname: this.state.fullname, umwid: this.state.umwid, accType: this.state.accType, email: this.state.email});
+
+                return fetch(`http://35.192.57.209:8000/testCreate`,
+                {method: "POST",
+                headers: header,
+                body: searchParams }).then(function(resp){
+                        return resp.json();
+                });
+  	}
 }
 
 export default AdminPage;
