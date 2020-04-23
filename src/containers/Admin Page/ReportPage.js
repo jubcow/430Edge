7268f564a51bbad5ad5e392882e374c7399eab9e
root <root@instance-1.us-central1-a.c.cpsc430-272902.internal>
2020-04-23 18:37:15 +0000

    Finished the dynamic chart of tasks on pagereport.js

--- src/containers/Admin Page/ReportPage.js
+++ src/containers/Admin Page/ReportPage.js
@@ -6,19 +6,6 @@ import Button from 'react-bootstrap/Button';
 import { PersonPlus,FilePost,Search } from 'react-bootstrap-icons';
 import {Bar} from 'react-chartjs-2';
 
-        const state = {
-                labels: ['January', 'February', 'March','April', 'May'],
-                datasets: [
-                {
-                        label: 'Rainfall',
-                        backgroundColor: 'rgba(75,192,192,1)',
-                        borderColor: 'rgba(0,0,0,1)',
-                        borderWidth: 2,
-                        data: [65, 59, 80, 81, 56]
-                }
-                ]
-        }
-
 class ReportPage extends React.Component {
         constructor(props) {
                 super(props);
@@ -26,29 +13,33 @@ class ReportPage extends React.Component {
                         info: [],
 			infoTasks: [],
                         adminName: null,
-			chartState: {
-				labels: ['January', 'February', 'March','April', 'May'],
-                		datasets: [
-                		{       
-                        		label: 'Rainfall',
-                        		backgroundColor: 'rgba(75,192,192,1)',
-                        		borderColor: 'rgba(0,0,0,1)',
-                        		borderWidth: 2,
-                        		data: [65, 59, 80, 81, 56]
-                		}
-                		]
-			} 
+			chartStateAlt: {
+                       		labels: ['Tasks', 'Unsubmitted', 'Submitted'],
+                        	datasets: [
+                        	{
+                                	label: 'TaskData',
+                                	backgroundColor: 'rgba(75,192,192,1)',
+                                	borderColor: 'rgba(0,0,0,1)',
+                                	borderWidth: 2,
+                                	data: []
+                        	}
+                        	]
+                	}
         	}
 	}
-
+	printStuff(){
+		console.log("test");
+	}
+	
         componentDidMount() {
                 let self = this;
                 var loadedName = jsCookie.get("fullName");
 
                 this.setState({
-                        adminName: loadedName
+                        adminName: loadedName,
                 });
 
+		var testData;
 		fetch('http://35.192.57.209:8000/reportUsers', {
                         method: 'GET'
                         }).then(function(response) {
@@ -63,6 +54,7 @@ class ReportPage extends React.Component {
                         	console.log('caught it!',err);
                         })
 
+		/*
 		fetch('http://35.192.57.209:8000/reportTasks', {
                         method: 'GET'
                         }).then(function(response) {
@@ -71,11 +63,74 @@ class ReportPage extends React.Component {
                                 }
                                 return response.json();
                         }).then(function(data) {
-                                console.log(data);
+                                //console.log(data);
+				testData = data;
+				console.log(testData);
+				console.log(data);
+				console.log(data[0].taskTotal);
                                 self.setState({infoTasks: data});
                         }).catch(err => {
                                 console.log('caught it!',err);
                         })
+		*/
+
+		fetch('http://35.192.57.209:8000/reportTasks', {
+                        method: 'GET'
+                        }).then(function(response) {
+                                if (response.status >= 400) {
+                                        throw new Error("Bad response from server");
+                                }
+                                return response.json();
+                        }).then(function(data) {
+                                //console.log(data);
+                                testData = data;
+                                console.log(testData);
+                                console.log(data);
+                                console.log(data[0].taskTotal);
+                                self.setState({
+					chartStateAlt: {
+                                		labels: ['Tasks', 'Unsubmitted', 'Submitted'],
+                                		datasets: [
+                                		{
+                                        		label: 'TaskData',
+                                        		backgroundColor: 'rgba(75,192,192,1)',
+                                        		borderColor: 'rgba(0,0,0,1)',
+                                        		borderWidth: 2,
+                                        		data: [data[0].taskTotal,data[0].taskUnsubmitted,data[0].taskSubmitted]
+                                		}
+                                		]
+                        		}	
+				});
+                        })
+
+		//console.log(this.testData[0]);
+		/*
+		console.log(testData);
+	        console.log(this.state.infoTasks);	
+		console.log(this.state.infoTasks.map(infoTasks => infoTasks.taskTotal));
+		console.log(this.state.infoTasks.taskTotal);
+		*/
+
+		/*
+		console.log(typeof this.state.infoTasks.taskTotal == 'undefined');
+		if(typeof this.state.infoTasks.infoTasks == 'undefined'){
+			console.log("returning");
+			console.log(this.state.infoTasks.taskTotal);
+		}
+		*/
+
+		this.chartState = {
+                	labels: ['Tasks', 'Unsubmitted', 'Submitted'],
+                	datasets: [
+                	{
+                        	label: 'TaskData',
+                        	backgroundColor: 'rgba(75,192,192,1)',
+                        	borderColor: 'rgba(0,0,0,1)',
+                        	borderWidth: 2,
+                        	data: []
+                	}
+                	]
+       	 	}
         }
 
 	render() {
@@ -100,21 +155,21 @@ class ReportPage extends React.Component {
 			Total Tasks: {this.state.infoTasks.map(infoTasks => <div>{infoTasks.taskTotal}</div>)}
 			Total Submitted: {this.state.infoTasks.map(infoTasks => <div>{infoTasks.taskSubmitted}</div>)}
 			Total Unsubmitted: {this.state.infoTasks.map(infoTasks => <div>{infoTasks.taskUnsubmitted}</div>)}
-			{this.state.chartState.datasets.data}
-<Bar
-          data={this.state.chartState}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
+
+			<Bar
+          			data={this.state.chartStateAlt}
+          			options={{
+            				title:{
+              					display:true,
+              					text:'Task Data',
+              					fontSize:20
+            				},
+            				legend:{
+              					display:true,
+              					position:'right'
+            				},
+          			}}
+        		/>
 
 			</div>
 
